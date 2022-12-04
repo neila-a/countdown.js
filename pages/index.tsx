b87ee29e3f773bdaa800d079ba63008a38c51cdf
neila-a <neilaspace@outlook.com>
2022-12-04 17:41:27 +0800

    添加<title>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,7 @@
-import { styled } from '@mui/material/styles';
-import {
-    Dialog,
-    Button,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    IconButton,
-    Typography
-} from '@mui/material';
-import {
-    Close as CloseIcon
-} from "@mui/icons-material";
+import Head from "next/head";
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-        padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-        padding: theme.spacing(1),
-    },
-}));
-export interface DialogTitleProps {
-    id: string;
-    children?: React.ReactNode;
-    onClose: () => void;
-}
-function BootstrapDialogTitle(props: DialogTitleProps) {
-    const { children, onClose, ...other } = props;
-    return (
-        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-            {children}
-            {onClose ? (
-                <IconButton
-                    aria-label="close"
-                    onClick={onClose}
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            ) : null}
-        </DialogTitle>
-    );
-}
 function Index(): JSX.Element {
     var [todayPassHours, setTodayPassHours] = useState<number>(1);
     var [todayPassHoursPercent, setTodayPassHoursPercent] = useState<number>(1);
@@ -113,102 +66,70 @@ function Index(): JSX.Element {
         setYearPass
     ]);
     const [open, setOpen] = React.useState(false);
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
     return (
-        <div className="sidebar-box classNameListBox">
-            <div className="aside aside-count">
-                <div className="p-3">
-                    <span style={{
-                        fontSize: "1.2em",
-                        color: "orange",
-                        display: "inline-block"
-                    }}>
-                        <i className="fas fa-hourglass-half"></i></span> 人生倒计时
-                    <img title="info" src="info.png" onClick={handleClickOpen} style={{
-                        display: "inline-block",
-                        float: "right"
-                    }} />
-                    <div>
-                        <BootstrapDialog
-                            onClose={handleClose}
-                            aria-labelledby="customized-dialog-title"
-                            open={open}
-                        >
-                            <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
-                                关于
-                            </BootstrapDialogTitle>
-                            <DialogContent dividers>
-                                <Typography gutterBottom>
-                                    此网页是
-                                    <a href="https://neila.ga/">Neila</a>
-                                    根据<a href="https://www.baixiaomo360.com/2">
-                                        白先生博客上的一篇文章</a>改编成的静态网页。
-                                    <a href="https://creativecommons.org/licenses/by/4.0/deed.zh">原代码协议</a>
-                                    {"   |   "}
-                                    <a href="https://www.gnu.org/licenses/gpl-3.0.html">本代码开源协议</a>
-                                </Typography>
-                            </DialogContent>
-                            <DialogActions>
-                                <Button autoFocus onClick={handleClose}>
-                                    关闭
-                                </Button>
-                            </DialogActions>
-                        </BootstrapDialog>
+        <>
+            <Head>
+                <title>人生倒计时</title>
+            </Head>
+            <div className="sidebar-box classNameListBox">
+                <div className="aside aside-count">
+                    <div className="p-3">
+                        <span style={{
+                            fontSize: "1.2em",
+                            color: "orange",
+                            display: "inline-block"
+                        }}>
+                            <i className="fas fa-hourglass-half"></i></span> 人生倒计时
                     </div>
-                </div>
-                <div className="content">
-                    <div className="item" id="dayProgress">
-                        <div className="title">今日已经过去<span>{todayPassHours}</span>小时</div>
-                        <div className="progress">
-                            <div className="progress-bar">
-                                <div className="progress-inner progress-inner-1" style={{
-                                    width: `${todayPassHoursPercent}%`
-                                }}></div>
+                    <div className="content">
+                        <div className="item" id="dayProgress">
+                            <div className="title">今日已经过去<span>{todayPassHours}</span>小时</div>
+                            <div className="progress">
+                                <div className="progress-bar">
+                                    <div className="progress-inner progress-inner-1" style={{
+                                        width: `${todayPassHoursPercent}%`
+                                    }}></div>
+                                </div>
+                                <div className="progress-percentage">{todayPassHoursPercent}%</div>
                             </div>
-                            <div className="progress-percentage">{todayPassHoursPercent}%</div>
                         </div>
-                    </div>
-                    <div className="item" id="weekProgress">
-                        <div className="title">这周已经过去<span>{weekDay}</span>天</div>
-                        <div className="progress">
-                            <div className="progress-bar">
-                                <div className="progress-inner progress-inner-2" style={{
-                                    width: `${weekDayPassPercent}%`
-                                }}></div>
+                        <div className="item" id="weekProgress">
+                            <div className="title">这周已经过去<span>{weekDay}</span>天</div>
+                            <div className="progress">
+                                <div className="progress-bar">
+                                    <div className="progress-inner progress-inner-2" style={{
+                                        width: `${weekDayPassPercent}%`
+                                    }}></div>
+                                </div>
+                                <div className="progress-percentage">{weekDayPassPercent}%</div>
                             </div>
-                            <div className="progress-percentage">{weekDayPassPercent}%</div>
                         </div>
-                    </div>
-                    <div className="item" id="monthProgress">
-                        <div className="title">本月已经过去<span>{date}</span>天</div>
-                        <div className="progress">
-                            <div className="progress-bar">
-                                <div className="progress-inner progress-inner-3" style={{
-                                    width: `${monthPassPercent}%`
-                                }}></div>
+                        <div className="item" id="monthProgress">
+                            <div className="title">本月已经过去<span>{date}</span>天</div>
+                            <div className="progress">
+                                <div className="progress-bar">
+                                    <div className="progress-inner progress-inner-3" style={{
+                                        width: `${monthPassPercent}%`
+                                    }}></div>
+                                </div>
+                                <div className="progress-percentage">{monthPassPercent}%</div>
                             </div>
-                            <div className="progress-percentage">{monthPassPercent}%</div>
                         </div>
-                    </div>
-                    <div className="item" id="yearProgress">
-                        <div className="title">今年已经过去<span>{month}</span>个月</div>
-                        <div className="progress">
-                            <div className="progress-bar">
-                                <div className="progress-inner progress-inner-4" style={{
-                                    width: `${yearPass}%`
-                                }}></div>
+                        <div className="item" id="yearProgress">
+                            <div className="title">今年已经过去<span>{month}</span>个月</div>
+                            <div className="progress">
+                                <div className="progress-bar">
+                                    <div className="progress-inner progress-inner-4" style={{
+                                        width: `${yearPass}%`
+                                    }}></div>
+                                </div>
+                                <div className="progress-percentage">{yearPass}%</div>
                             </div>
-                            <div className="progress-percentage">{yearPass}%</div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 export default Index;
